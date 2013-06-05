a58b696f4292cdce4161faf670a8f60f71bbb53e
John Persano <johnpersano@gmail.com>
2013-06-04 20:46:57 -0400

    Added utilities package.

---
+++ LibrarySuperToasts/src/com/extlibsupertoasts/utilities/SuperToastConstants.java
@@ -0,0 +1,41 @@
+package com.extlibsupertoasts.utilities;
+
+import com.extlibsupertoasts.R;
+
+public class SuperToastConstants 
+{
+	
+	public static final int BACKGROUND_WHITE = (R.drawable.background_white);
+	public static final int BACKGROUND_BLACK = (R.drawable.background_black);
+	public static final int BACKGROUND_ORANGE = (R.drawable.background_orange);
+	public static final int BACKGROUND_PURPLE = (R.drawable.background_purple);
+	public static final int BACKGROUND_YELLOW = (R.drawable.background_yellow);
+	public static final int BACKGROUND_BLUE = (R.drawable.background_blue);
+	public static final int BACKGROUND_HOLOBLUE = (R.drawable.background_holoblue);
+	
+	
+	public static final int DURATION_SHORT = (2000);
+	public static final int DURATION_MEDIUM = (2750);
+	public static final int DURATION_LONG = (3500);
+	public static final int DURATION_XLONG = (4500);
+	
+	
+	public static final String FONT_ROBOTO_BOLD = ("Roboto-Bold.ttf");
+	public static final String FONT_ROBOTO_BOLD_ITALIC = ("Roboto-BoldItalic.ttf");
+	public static final String FONT_ROBOTO_CONDENSED = ("Roboto-Condensed.ttf");
+	public static final String FONT_ROBOTO_CONDENSED_ITALIC = ("Roboto-CondensedItalic.ttf");
+	public static final String FONT_ROBOTO_ITALIC= ("Roboto-Italic.ttf");
+	public static final String FONT_ROBOTO_LIGHT = ("Roboto-Light.ttf");
+	public static final String FONT_ROBOTO_LIGHT_ITALIC = ("Roboto-LightItalic.ttf");
+	public static final String FONT_ROBOTO_MEDIUM = ("Roboto-Medium.ttf");
+	public static final String FONT_ROBOTO_MEDIUM_ITALIC = ("Roboto-MediumItalic.ttf");
+	public static final String FONT_ROBOTO_REGULAR = ("Roboto-Regular.ttf");
+	public static final String FONT_ROBOTO_THIN = ("Roboto-Thin.ttf");
+	public static final String FONT_ROBOTO_THIN_ITALIC = ("Roboto-ThinItalic.ttf");
+	
+	
+	public static final int TEXTSIZE_SMALL = (14);
+	public static final int TEXTSIZE_MEDIUM = (18);
+	public static final int TEXTSIZE_LARGE = (22);
+
+}
