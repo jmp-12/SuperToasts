888e6fd3122c1c69318863eb56196467a2a34da5
John Persano <johnpersano@gmail.com>
2013-07-16 20:05:49 -0400

    Initial commit.

---
+++ SuperToasts-Library/SuperToastsLibrary/src/com/extlibsupertoasts/utilities/OnDismissListener.java
@@ -0,0 +1,9 @@
+package com.extlibsupertoasts.utilities;
+
+/** Listens for dismiss events from SuperToasts. */
+public interface OnDismissListener {
+	
+	/** Called when a SuperToast is dismissed. */
+	public void onDismiss();
+
+}
