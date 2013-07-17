ec51ca90c5974272e25f2055f8af284e034c5cec
John Persano <johnpersano@gmail.com>
2013-07-16 20:10:46 -0400

    Moving folders around.

--- SuperToasts-Library/SuperToastsLibrary/src/com/extlibsupertoasts/utilities/OnDismissListener.java
+++
@@ -1,9 +0,0 @@
-package com.extlibsupertoasts.utilities;
-
-/** Listens for dismiss events from SuperToasts. */
-public interface OnDismissListener {
-	
-	/** Called when a SuperToast is dismissed. */
-	public void onDismiss();
-
-}
