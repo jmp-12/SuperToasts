824090c3553fbec9dc31da25ab7b3fcf9bb5d02a
John Persano <johnpersano@gmail.com>
2013-07-17 17:52:18 -0400

    Started new demo.

--- SuperToasts-Demo/src/com/supertoastsdemo/MainActivity.java
+++ SuperToasts-Demo/src/com/supertoastsdemo/MainActivity.java
@@ -1,26 +1,65 @@
 package com.supertoastsdemo;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.extlibsupertoasts.SuperToast;
+
+public class MainActivity extends SherlockActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Test
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(actionBar.getThemedContext(),
+                android.R.layout.simple_spinner_item, android.R.id.text1, getResources().getStringArray(R.array.navigation_list));
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        actionBar.setListNavigationCallbacks(arrayAdapter, new ActionBar.OnNavigationListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+
+                switch (itemPosition) {
+
+                    case 0:
+
+                        break;
+
+
+                    case 1:
+
+                        break;
+
+
+                    case 2:
+
+                        break;
+
+                }
+
+                return false;
+
+            }
+        });
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+
     }
-    
+
+
 }
