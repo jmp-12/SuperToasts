017c0e6738f6830fe02bcf60d75b962503cd08ac
John Persano <johnpersano@gmail.com>
2013-07-16 20:11:14 -0400

    Moving folders around

---
+++ SuperToasts-Library/src/com/extlibsupertoasts/ManagerSuperActivityToast.java
@@ -0,0 +1,331 @@
+/**
+ *  Copyright 2013 John Persano
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ * 
+ */
+
+package com.extlibsupertoasts;
+
+import java.util.Iterator;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+/** Manages the life of a SuperActivityToast. Copied from the Crouton library */
+public class ManagerSuperActivityToast extends Handler {
+	
+	private static final String TAG = "ManagerSuperActivityToast";
+
+	private static final class Messages {
+
+		public static final int DISPLAY_SUPERACTIVITYTOAST = 0xc2007;
+		public static final int ADD_SUPERACTIVITYTOAST = 0xc20074dd;
+		public static final int REMOVE_SUPERACTIVITYTOAST = 0xc2007de1;
+
+		private Messages() {
+
+			// Do nothing
+
+		}
+
+	}
+
+	private static ManagerSuperActivityToast manager;
+
+	private Queue<SuperActivityToast> toastQueue;
+
+	private ManagerSuperActivityToast() {
+
+		toastQueue = new LinkedBlockingQueue<SuperActivityToast>();
+
+	}
+
+	protected static synchronized ManagerSuperActivityToast getInstance() {
+
+		if (manager != null) {
+
+			return manager;
+
+		} else {
+
+			manager = new ManagerSuperActivityToast();
+
+			return manager;
+
+		}
+
+	}
+	
+
+	protected void add(SuperActivityToast superActivityToast) {
+
+		toastQueue.add(superActivityToast);
+		this.showNextSuperToast();
+
+	}
+
+	
+	private void showNextSuperToast() {
+
+		if (toastQueue.isEmpty()) {
+
+			return;
+
+		}
+
+		final SuperActivityToast superActivityToast = toastQueue.peek();
+
+		if(superActivityToast.getActivity() == null) {
+
+			toastQueue.poll();
+
+		}
+
+		if (!superActivityToast.isShowing()) {
+
+			sendMessage(superActivityToast, Messages.ADD_SUPERACTIVITYTOAST);
+
+		} else {
+
+			sendMessageDelayed(superActivityToast,
+					Messages.DISPLAY_SUPERACTIVITYTOAST,
+					getDuration(superActivityToast));
+
+		}
+
+	}
+
+	
+	private void sendMessage(SuperActivityToast superActivityToast,
+			final int messageId) {
+
+		final Message message = obtainMessage(messageId);
+		message.obj = superActivityToast;
+		sendMessage(message);
+
+	}
+
+	
+	private void sendMessageDelayed(SuperActivityToast superActivityToast,
+			final int messageId, final long delay) {
+
+		Message message = obtainMessage(messageId);
+		message.obj = superActivityToast;
+		sendMessageDelayed(message, delay);
+
+	}
+
+	
+	private long getDuration(SuperActivityToast superActivityToast) {
+
+		long duration = superActivityToast.getDuration();
+		duration += superActivityToast.getShowAnimation().getDuration();
+		duration += superActivityToast.getDismissAnimation().getDuration();
+
+		return duration;
+
+	}
+
+	@Override
+	public void handleMessage(Message message) {
+
+		final SuperActivityToast superActivityToast = (SuperActivityToast) 
+				message.obj;
+
+		switch (message.what) {
+
+			case Messages.DISPLAY_SUPERACTIVITYTOAST:
+	
+				showNextSuperToast();
+	
+				break;
+	
+			case Messages.ADD_SUPERACTIVITYTOAST:
+	
+				displaySuperToast(superActivityToast);
+	
+				break;
+	
+			case Messages.REMOVE_SUPERACTIVITYTOAST:
+	
+				removeSuperToast(superActivityToast);
+	
+				break;
+	
+			default: {
+	
+				super.handleMessage(message);
+	
+				break;
+
+			}
+
+		}
+
+	}
+
+	private void displaySuperToast(SuperActivityToast superActivityToast) {
+
+		if (superActivityToast.isShowing()) {
+
+			return;
+
+		}
+
+		final ViewGroup viewGroup = superActivityToast.getViewGroup();
+
+		final View toastView = superActivityToast.getView();
+		
+		//TODO: Temporary fix for orientation change crash.
+		if(viewGroup != null) {
+			
+			try {
+				
+				viewGroup.addView(toastView);
+
+				toastView.startAnimation(superActivityToast.getShowAnimation());
+				
+			} catch(IllegalStateException e) {
+				
+				Log.e(TAG, e.toString());		
+				
+				clearSuperActivityToastsForActivity(superActivityToast.getActivity());
+
+			}
+
+		}
+
+		
+		if(!superActivityToast.getIsIndeterminate()) {
+			
+			sendMessageDelayed(superActivityToast, Messages.REMOVE_SUPERACTIVITYTOAST,
+					superActivityToast.getDuration() + superActivityToast.getShowAnimation().getDuration());
+			
+		}
+
+	}
+
+	
+	protected void removeSuperToast(SuperActivityToast superActivityToast) {
+
+		final ViewGroup viewGroup = superActivityToast.getViewGroup();
+
+		final View toastView = superActivityToast.getView();
+
+		if (viewGroup != null) {
+
+			toastView.startAnimation(superActivityToast.getDismissAnimation());
+
+			toastQueue.poll();
+
+			viewGroup.removeView(toastView);
+
+			sendMessageDelayed(superActivityToast,
+					Messages.DISPLAY_SUPERACTIVITYTOAST, superActivityToast
+							.getDismissAnimation().getDuration());
+			
+			if(superActivityToast.getOnDismissListener() != null) {
+				
+				superActivityToast.getOnDismissListener().onDismiss();
+				
+			}
+
+		}
+
+	}
+	
+
+	protected void clearQueue() {
+
+		removeAllMessages();
+
+		if (toastQueue != null) {
+
+			for (SuperActivityToast superActivityToast : toastQueue) {
+
+				if (superActivityToast.isShowing()) {
+
+					superActivityToast.getViewGroup().removeView(
+							superActivityToast.getView());
+
+				}
+
+			}
+
+			toastQueue.clear();
+
+		}
+
+	}
+
+	protected void clearSuperActivityToastsForActivity(Activity activity) {
+
+		if (toastQueue != null) {
+
+			Iterator<SuperActivityToast> superActivityToastIterator = toastQueue
+					.iterator();
+
+			while (superActivityToastIterator.hasNext()) {
+
+				SuperActivityToast superActivityToast = superActivityToastIterator
+						.next();
+
+				if ((superActivityToast.getActivity()) != null
+						&& superActivityToast.getActivity().equals(activity)) {
+
+					if (superActivityToast.isShowing()) {
+
+						superActivityToast.getViewGroup().removeView(
+								superActivityToast.getView());
+
+					}
+
+					removeAllMessagesForSuperActivityToast(superActivityToast);
+
+					superActivityToastIterator.remove();
+
+				}
+
+			}
+
+		}
+
+	}
+
+	
+	private void removeAllMessages() {
+
+		removeMessages(Messages.ADD_SUPERACTIVITYTOAST);
+		removeMessages(Messages.DISPLAY_SUPERACTIVITYTOAST);
+		removeMessages(Messages.REMOVE_SUPERACTIVITYTOAST);
+
+	}
+
+	
+	private void removeAllMessagesForSuperActivityToast(
+			SuperActivityToast superActivityToast) {
+		removeMessages(Messages.ADD_SUPERACTIVITYTOAST, superActivityToast);
+		removeMessages(Messages.DISPLAY_SUPERACTIVITYTOAST, superActivityToast);
+		removeMessages(Messages.REMOVE_SUPERACTIVITYTOAST, superActivityToast);
+
+	}
+
+}
---
+++ SuperToasts-Library/src/com/extlibsupertoasts/ManagerSuperToast.java
@@ -0,0 +1,255 @@
+/**
+ *  Copyright 2013 John Persano
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ * 
+ */
+
+package com.extlibsupertoasts;
+
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.view.WindowManager;
+
+/** Manages the life of a SuperActivityToast. Copied from the Crouton library */
+public class ManagerSuperToast extends Handler {
+
+	private static final class Messages {
+		
+		//TODO:
+		public static final int DISPLAY_SUPERTOAST = 0xc2008;
+		public static final int ADD_SUPERTOAST = 0xc20084dd;
+		public static final int REMOVE_SUPERTOAST = 0xc2008de1;
+
+		private Messages() {
+
+			// Do nothing
+
+		}
+
+	}
+
+	private static ManagerSuperToast manager;
+
+	private Queue<SuperToast> toastQueue;
+
+	private ManagerSuperToast() {
+
+		toastQueue = new LinkedBlockingQueue<SuperToast>();
+
+	}
+
+	protected static synchronized ManagerSuperToast getInstance() {
+
+		if (manager != null) {
+
+			return manager;
+
+		} else {
+
+			manager = new ManagerSuperToast();
+
+			return manager;
+
+		}
+
+	}
+	
+
+	protected void add(SuperToast superToast) {
+
+		toastQueue.add(superToast);
+		this.showNextSuperToast();
+
+	}
+
+	
+	private void showNextSuperToast() {
+
+		if (toastQueue.isEmpty()) {
+
+			return;
+
+		}
+
+		final SuperToast superToast = toastQueue.peek();
+
+		if (!superToast.isShowing()) {
+
+			sendMessage(superToast, Messages.ADD_SUPERTOAST);
+
+		} else {
+
+			sendMessageDelayed(superToast,
+					Messages.DISPLAY_SUPERTOAST,
+					getDuration(superToast));
+
+		}
+
+	}
+
+	private void sendMessage(SuperToast superToast,
+			final int messageId) {
+
+		final Message message = obtainMessage(messageId);
+		message.obj = superToast;
+		sendMessage(message);
+
+	}
+
+	private void sendMessageDelayed(SuperToast superToast,
+			final int messageId, final long delay) {
+
+		Message message = obtainMessage(messageId);
+		message.obj = superToast;
+		sendMessageDelayed(message, delay);
+
+	}
+	
+	private long getDuration(SuperToast superToast) {
+
+		long duration = superToast.getDuration();
+		duration += 1000;
+		
+		return duration;
+
+	}
+
+	@Override
+	public void handleMessage(Message message) {
+
+		final SuperToast superToast = (SuperToast) 
+				message.obj;
+
+		switch (message.what) {
+
+			case Messages.DISPLAY_SUPERTOAST:
+	
+				showNextSuperToast();
+	
+				break;
+	
+			case Messages.ADD_SUPERTOAST:
+	
+				displaySuperToast(superToast);
+	
+				break;
+	
+			case Messages.REMOVE_SUPERTOAST:
+	
+				removeSuperToast(superToast);
+	
+				break;
+	
+			default: {
+	
+				super.handleMessage(message);
+	
+				break;
+
+			}
+
+		}
+
+	}
+
+	private void displaySuperToast(SuperToast superToast) {
+
+		if (superToast.isShowing()) {
+
+			return;
+
+		}
+
+		final WindowManager windowManager = superToast
+				.getWindowManager();
+
+		final View toastView = superToast.getView();
+		
+		final WindowManager.LayoutParams params = superToast
+				.getWindowManagerParams();
+		
+		if(windowManager != null) {
+			
+			windowManager.addView(toastView, params);
+
+		}
+			
+		sendMessageDelayed(superToast, Messages.REMOVE_SUPERTOAST,
+				superToast.getDuration() + 500);
+			
+	}
+	
+	protected void removeSuperToast(SuperToast superToast) {
+		
+		final WindowManager windowManager = superToast
+				.getWindowManager();
+
+		final View toastView = superToast.getView();
+
+		if (windowManager != null) {
+
+			toastQueue.poll();
+
+			windowManager.removeView(toastView);
+
+			sendMessageDelayed(superToast,
+					Messages.DISPLAY_SUPERTOAST, 500);
+			
+			if(superToast.getOnDismissListener() != null) {
+				
+				superToast.getOnDismissListener().onDismiss();
+				
+			}
+
+		}
+
+	}
+	
+	protected void clearQueue() {
+
+		removeAllMessages();
+
+		if (toastQueue != null) {
+
+			for (SuperToast superToast : toastQueue) {
+
+				if (superToast.isShowing()) {
+
+					superToast.getWindowManager().removeView(
+							superToast.getView());
+
+				}
+
+			}
+
+			toastQueue.clear();
+
+		}
+
+	}
+	
+	private void removeAllMessages() {
+
+		removeMessages(Messages.ADD_SUPERTOAST);
+		removeMessages(Messages.DISPLAY_SUPERTOAST);
+		removeMessages(Messages.REMOVE_SUPERTOAST);
+
+	}
+
+}
