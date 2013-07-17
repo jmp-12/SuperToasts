deacfcf51d4d438ab011fe633a2e1fb33131aa09
John Persano <johnpersano@gmail.com>
2013-07-16 23:32:56 -0400

    Started new demo.

--- SuperToasts-Demo/src/com/supertoastsdemo/MainActivity.java
+++ SuperToasts-Demo/src/com/supertoastsdemo/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Test
+
     }
 
 
