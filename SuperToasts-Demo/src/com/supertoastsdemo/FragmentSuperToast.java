f2a9e221c0b1acf0356fecf56a78dcccf0709cda
John Persano <johnpersano@gmail.com>
2013-07-20 21:34:52 -0400

    Added test image.

--- SuperToasts-Demo/src/com/supertoastsdemo/FragmentSuperToast.java
+++ SuperToasts-Demo/src/com/supertoastsdemo/FragmentSuperToast.java
@@ -1,6 +1,7 @@
 package com.supertoastsdemo;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
