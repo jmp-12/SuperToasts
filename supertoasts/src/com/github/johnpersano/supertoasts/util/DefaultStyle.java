1510a76abe7208c02fd25c6695b02f472d739a41
John Persano <johnpersano@gmail.com>
2014-02-03 17:16:41 +0000

    Added DefaultStyle class.

---
+++ supertoasts/src/com/github/johnpersano/supertoasts/util/DefaultStyle.java
@@ -0,0 +1,13 @@
+package com.github.johnpersano.supertoasts.util;
+
+import com.github.johnpersano.supertoasts.SuperToast;
+
+public class DefaultStyle {
+
+    public SuperToast.Animations animations;
+    public int duration;
+    public int background;
+    public int typefaceStyle;
+    public int textColor;
+
+}
