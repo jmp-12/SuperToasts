354be380ad3942d8f8bf157811b981cc9340ebd5
John Persano <johnpersano@gmail.com>
2014-02-08 21:29:58 +0000

    Added Playground class.

--- demo/src/com/supertoastsdemo/Playground.java
+++ demo/src/com/supertoastsdemo/Playground.java
@@ -2,10 +2,12 @@ package com.supertoastsdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import com.github.johnpersano.supertoasts.SuperActivityToast;
 import com.github.johnpersano.supertoasts.SuperToast;
+import com.github.johnpersano.supertoasts.util.*;
+
 
 /** Use this class to play around with the library. Simply run this activity
  *  in a specified run configuration */
@@ -15,8 +17,6 @@ public class Playground extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playground);
 
-        SuperActivityToast.onRestoreState(savedInstanceState, this);
-
         final Button showButton = (Button)
                 findViewById(R.id.show_button);
         showButton.setOnClickListener(new View.OnClickListener() {
@@ -24,21 +24,24 @@ public class Playground extends Activity {
             @Override
             public void onClick(View view) {
 
-                SuperActivityToast.createSuperActivityToast(Playground.this, "Time to play!",
-                        SuperToast.Duration.SHORT).show();
+                final SuperToast superToast = new SuperToast(Playground.this, Style.getStyle(Style.GREEN));
+                superToast.setDuration(4500);
+                superToast.setOnDismissListener(new OnDismissListener() {
+
+                    @Override
+                    public void onDismiss(View view) {
+
+                        Log.e("SuperToast", "On Dismiss");
+
+                    }
+                });
+
+                superToast.show();
 
             }
         });
 
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        /** This method is used to save any showing or pending SuperActivityToasts */
-        SuperActivityToast.onSaveState(outState);
-
-    }
 
 }
